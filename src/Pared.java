8dea6bdee2e325e63e40a391efb09315e5d65c80
tadeowoto <tadeo.woto@gmail.com>
2024-03-19 14:17:01 -0300

    adding the wall class

---
+++ src/Pared.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author tDev
+ */
+public class Pared {
+    int posX;
+    int posY;
+
+    public Pared() {
+    }
+
+    public Pared(int posX, int posY) {
+        this.posX = posX;
+        this.posY = posY;
+    }
+
+    public int getPosX() {
+        return posX;
+    }
+    public int getPosY() {
+        return posY;
+    }
+
+}
